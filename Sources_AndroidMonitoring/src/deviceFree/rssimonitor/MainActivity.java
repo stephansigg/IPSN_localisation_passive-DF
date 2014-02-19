08189945f565e83e8633d3a3aca49d05265fec65
Stephan <tuffi78@gmx.de>
2014-02-19 12:58:28 +0100

    14.02.19

--- Sources_AndroidMonitoring/src/deviceFree/rssimonitor/MainActivity.java
+++ Sources_AndroidMonitoring/src/deviceFree/rssimonitor/MainActivity.java
@@ -24,71 +24,21 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-//	public void startSetup(View view){
-//		// sh /sdcard/setup.sh
-//	    Process p; 
-//	    try {   
-//	       // Preform su to get root privledges  
-//	       p = Runtime.getRuntime().exec("su");   
-//	       DataOutputStream os = new DataOutputStream(p.getOutputStream());   
-//	       os.writeBytes("sh /sdcard/setup.sh > /sdcard/setupOutput.txt\n");
-//	       os.flush();
-//	       os.close();
-//	       //os.writeBytes("sh /sdcard/setup.sh\n")
-//	       p.waitFor();
-//	    } catch (IOException e) {   
-//	       // TODO Code to run in input/output exception  
-//	        //toastMessage("not root");   
-//	    } catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	    
-//	    Process p2;
-//	    try{
-//	       p2 = Runtime.getRuntime().exec("su");
-//	       DataOutputStream os2 = new DataOutputStream(p2.getOutputStream());   
-//	       os2.writeBytes("tcpdump eth0 -w /sdcard/myTestOutput140207.cap\n");
-//	       os2.flush();
-//	       os2.close();
-//		} catch (IOException e) {   
-//	       // TODO Code to run in input/output exception  
-//	        //toastMessage("not root");   
-//	    }
-//	    
-//		Process pSetup;
-//		try {
-//			pSetup = Runtime.getRuntime().exec("sh /sdcard/setup.sh");
-//		}catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			}
-//		//BufferedReader in = new BufferedReader(new InputStreamReader(pSetup.getInputStream()));
-//	}
-	
+		
 	public void startSetup(View view){
 		Thread th = new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				ArrayList<String> liste = new ArrayList<String>();
-				liste.add("sh /sdcard/setup.sh");
-				//liste.add("cd /sdcard/");
-				//liste.add("mkdir neuerOrdner");
+				liste.add("cd /sdcard/");
+				liste.add("sh ./setup.sh");
 				liste.add("sleep 10");
-				//liste.add("tcpdump eth0 -w /sdcard/myTestOutput140207.cap");
 				//liste.add("sh /sdcard/startTcpdump.sh &");
-				liste.add("tcpdump -w /sdcard/myTestOutput140208.cap");
-				//liste.add("ls > /sdcard/nochEinOutput.txt");
+				liste.add("tcpdump -w /sdcard/myTestOutput140207.cap &");
 				liste.add("sleep 20");
-				//liste.add("mv /sdcard/myTestOutput140208 /sdcard/myTestOutput_sample");
-				//liste.add("tcpdump -w /sdcard/myTestOutput140209.cap");
-				// sh /sdcard/setup.sh
-				//liste.add("killall tcpdump");
+				liste.add("killall tcpdump");
 				Shell.SU.run(liste);
-//			       Shell.SU.run("sh /sdcard/setup.sh > /sdcard/setupOutput.txt\n");
-//			       Shell.SU.run("tcpdump eth0 -w /sdcard/myTestOutput140207.cap\n");				
 			}
 		});
 		th.start();
@@ -96,28 +46,18 @@ public class MainActivity extends Activity {
 	
 	
 	public void startTcpdump(View view){
-		// sh /sdcard/setup.sh
-	    Process p2;   
-	    try {   
-	       // Preform su to get root privledges  
-	       p2 = Runtime.getRuntime().exec("su");   
-	         
-	       // Attempt to write a file to a root-only   
-	    } catch (IOException e) {   
-	       // TODO Code to run in input/output exception  
-	        //toastMessage("not root");   
-	    } 
-		
-		Process pTcpdump;
-		try {
-			pTcpdump = Runtime.getRuntime().exec("tcpdump -w /sdcard/myTestOutput4.txt");
-			DataOutputStream os = new DataOutputStream(pTcpdump.getOutputStream());   
-		    os.writeBytes("tcpdump -w /sdcard/myTestOutput3.txt");
-		}catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// This is not actually necessary. 
+		// Better: function to send the data via bluetooth somewhere
+		Thread th2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ArrayList<String> liste = new ArrayList<String>();
+				liste.add("killall tcpdump");
+				Shell.SU.run(liste);
 			}
-		//BufferedReader in = new BufferedReader(new InputStreamReader(pSetup.getInputStream()));
+		});
+		th2.start();
+		
 	}
 	
 }
